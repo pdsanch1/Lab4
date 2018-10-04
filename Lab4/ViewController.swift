7c70b60769c7898705c089c7056d8c4cdfbe346a
Pedro Daniel Sanchez A <pdsanch1@gmail.com>
2018-10-03 21:32:11 -0500

    Initial Commit

---
+++ Lab4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab4
+//
+//  Created by Pedro Daniel Sanchez on 10/3/18.
+//  Copyright © 2018 Pedro Daniel Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
